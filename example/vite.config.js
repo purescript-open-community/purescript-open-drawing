60b80c9b92df166c0eed5103834b92b71429bf2f
Serhii Khoma <sergey@nordicresults.com>
2025-08-20 16:28:01 +0700

    feat: monorepo -> single

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,15 +3,16 @@ import * as fs from 'fs'
 import * as yaml from 'js-yaml'
 
 // Read and parse the YAML file
-const spagoYamlFilePath = path.resolve(__dirname, 'lib', 'spago.yaml');
+const spagoYamlFilePath = path.resolve(__dirname, '..', 'spago.yaml');
 const spagoYamlFileContent = fs.readFileSync(spagoYamlFilePath, 'utf8');
 const data = yaml.load(spagoYamlFileContent);
 
 export default {
   base: `/${data.package.publish.location.githubRepo}/`,
-  root: './example',
+  // base: `/`,
+  root: '.',
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: path.resolve(__dirname, "..", "dist"),
   },
   mode: 'development',
   resolve: {
